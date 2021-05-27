1223e48ae493953789dd8574a59cda74770624de
hamzsait <hamzasait@utexas.edu>
2021-05-27 13:49:22 -0500

    changed search

--- webscraper/texasTribune.py
+++ webscraper/texasTribune.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome('chromedriver')
 driver.get("https://www.google.com")
 
 input = driver.find_element_by_xpath('//input[@type="text"]')
-input.send_keys("Barack Obama Gay ?")
+input.send_keys("Barack Obama")
 
 submit = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
 submit.click()
