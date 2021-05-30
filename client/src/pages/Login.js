558fd42c072db9306fcc076ebdc17449c7f9202b
sournachos <joserivera18100@gmail.com>
2021-05-29 22:33:12 -0500

    Moved Bootstrap container to its respective pages and took it off the App.js so that it doesnt't disrupt other pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import { Form, Button, Card, Alert, Container } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 
@@ -27,6 +27,10 @@ export default function Login() {
   }
 
   return (
+    <Container
+    className="d-flex align-items-center justify-content-center"
+    style={{ minHeight: "100vh" }}
+  >
     <div className="w-100" style={{ maxWidth: "400px" }}>
       <Card>
         <Card.Body>
@@ -54,5 +58,6 @@ export default function Login() {
         Need an account? <Link to="/signup">Sign Up</Link>
       </div>
     </div>
+    </Container>
   )
 }
