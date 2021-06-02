6066f8e7e0f2cea57c2b3037c1d4dd3643e01e1e
Matthew Parker <matt.parker1007@gmail.com>
2021-06-02 12:36:15 -0500

    map default zoom, text and flexbox

--- client/src/components/InteractiveMap/index.js
+++ client/src/components/InteractiveMap/index.js
@@ -7,20 +7,28 @@ export default function InteractiveMap() {
   return (
     <div className="map-box mb-5 mw-100 rounded">
         <TransformWrapper
-          defaultScale={1}
+          defaultScale={0.98}
           defaultPositionX={200}
           defaultPositionY={100}
           wheel={{ disabled: true }}
+          options={{ minScale: 0.98 }}
         >
           {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
             <React.Fragment>
               
-              <div className="tools d-flex flex-row justify-content-center align-items-center p-2" >
-                <div className="mr-4">
-                  <button onClick={resetTransform} className="zoom" id="zoom-reset">Reset</button>
+              <div className="tools d-flex flex-row justify-content-between align-items-center p-2" >
+                <div class="map-head">
+                  <h5 className="mr-5 my-0">U.S. Congressional Districts – Texas</h5>
+                  <i class="fas fa-info-circle"></i>
                 </div>
-                <button onClick={zoomOut} className="zoom" id="zoom-out"><i className="fas fa-search-minus" ></i></button>
-                <button onClick={zoomIn} className="zoom" id="zoom-in"><i className="fas fa-search-plus" ></i></button>
+
+                <div>
+              
+                  <button onClick={zoomOut} className="zoom" id="zoom-out"><i className="fas fa-search-minus" ></i></button>
+                  <button onClick={zoomIn} className="zoom" id="zoom-in"><i className="fas fa-search-plus" ></i></button>
+                  <button onClick={resetTransform} className="zoom ml-5" id="zoom-reset">Reset</button>
+                </div>
+                
                 
         
 
