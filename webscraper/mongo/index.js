a8e330c95c4c0247f4163ec9e4a6d7b70b825f4c
sournachos <joserivera18100@gmail.com>
2021-06-03 00:43:03 -0500

    Edited the index.js in the mongo folder to return an array of objects in which each object is a congressman

--- webscraper/mongo/index.js
+++ webscraper/mongo/index.js
@@ -1,7 +1,7 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config()
 
-async function main(uri){
+async function main(){
 
     var uri = `mongodb+srv://webuser:${process.env.DB_PASSWORD}@cluster0.gg0wl.mongodb.net/Cluster0?retryWrites=true&w=majority`
 
@@ -9,10 +9,24 @@ async function main(uri){
     await client.connect()
 
 
-    congress_collection = client.db('Cluster0').collection('texas_congress').find({});
-    congress_collection.forEach(congressman => console.log(congressman))
+     let congress_collection = client.db('Cluster0').collection('texas_congress').find({});
+
+     return main2(congress_collection, client)
+} 
+
+async function main2(congress_collection, client){
+
+    let output =[]
+
+   await congress_collection.forEach(congressman => output.push(congressman))
+
+   console.log(output)
 
     client.close()
+
+    return output;
 }
 
-main().catch(console.error);
+let pyScraper = main().catch(console.error);
+
+export default pyScraper;
