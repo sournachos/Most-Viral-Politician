1840848d8c8e1f84ae768a3c0c5c68ab8a3d65d7
Matthew Parker <matt.parker1007@gmail.com>
2021-06-03 14:50:42 -0500

    changed componentnav to a bootstrap list group

--- client/src/components/ComponentNav/index.js
+++ client/src/components/ComponentNav/index.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 import "./style.css";
 
 export default function ComponentNav({ headings }) {
   return (
-    <div class="container">
-      <div class="row">
-        {headings.map(heading => {
-          let link = heading.split(" ").join("");
-          return <a href={"#"+link}>{heading}</a>
-        })
-}
-      </div>
-    </div>
-  )
+//     <div class="container">
+//       <div class="row">
+//         {headings.map(heading => {
+//           let link = heading.split(" ").join("");
+//           return <a href={"#"+link}>{heading}</a>
+//         })
+// }
+//       </div>
+//     </div>
+  <ListGroup horizontal className="my-2">
+    {headings.map(heading => {
+      let link = heading.split(" ").join("");
+      return <a href={"#"+link} className="text-decoration-none">
+        <ListGroup.Item>{heading}</ListGroup.Item>
+        </a>
+    })}
+  </ListGroup>
+)
 }
