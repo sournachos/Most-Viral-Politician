75c64b1906ad6297a6572e12cb6b13ea41f6aa28
hamzsait <hamzasait@utexas.edu>
2021-06-04 10:31:11 -0500

    current version

--- webscraper/mongo/index.js
+++ webscraper/mongo/index.js
@@ -1,17 +1,23 @@
 const {MongoClient} = require('mongodb');
 require('dotenv').config()
 
+async function getDB(client){
+
+    const documents = await client.db('Cluster0').collection('texas_congress').find({})
+    
+    documents.map(document => console.log(document))
+    
+
+    return true
+}
+
 async function main(uri){
 
     var uri = `mongodb+srv://webuser:${process.env.DB_PASSWORD}@cluster0.gg0wl.mongodb.net/Cluster0?retryWrites=true&w=majority`
 
     const client = new MongoClient(uri)
     await client.connect()
-
-
-    congress_collection = client.db('Cluster0').collection('texas_congress').find({});
-    congress_collection.forEach(congressman => console.log(congressman))
-
+    const congress = await getDB(client)
     client.close()
 }
 
