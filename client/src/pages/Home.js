5ecc2868f759985434439192142d38221bb50eab
Matthew Parker <matt.parker1007@gmail.com>
2021-06-05 21:02:34 -0500

    moved api call to home.js and made DataContext

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ComponentNav from "../components/ComponentNav"
 import InteractiveMap from "../components/InteractiveMap";
 import { List, ListItem } from "../components/List"
 import { Button, Card } from "react-bootstrap"
 //import { Link} from "react-router-dom"
+import API from "../utils/API";
+import DataContext from "../contexts/DataContext";
 
 const red = "#953738";
 const tan = "#e3dfb0";
@@ -12,6 +14,17 @@ const lightblue = "#0A6281";
 const blue = "#0B406F";
 
 export default function Home() {
+  const [dataState, setDataState] = useState({});
+
+  useEffect(() => {
+    API.getPols()
+      .then(res => {
+        setDataState(res.data);
+      })
+      .catch(err => console.log(err));
+
+  }, [dataState])
+
   return (
     <>
     
@@ -23,8 +36,11 @@ export default function Home() {
 
       <Card>
         <Card.Body>
-        <InteractiveMap />
+        <DataContext.Provider value={dataState}>
+          <InteractiveMap />
           <List />
+        </DataContext.Provider>
+
         </Card.Body>
       </Card>
     </>
