b4458ab6f98ebfdc758364b1a83043875ddf324e
hamzsait <hamzasait@utexas.edu>
2021-06-06 18:09:04 -0500

    added function to update refresh facebook if nothing loads

--- webscraper/TexasHouseAPI/facebookStats.py
+++ webscraper/TexasHouseAPI/facebookStats.py
@@ -1,6 +1,14 @@
 from webdriver import getWebdriver
 from time import sleep
 
+def makeConnection(driver, link):
+    try:
+        sleep(30)
+        driver.get(link)
+        followers = driver.find_element_by_xpath("//*[contains(text(),'like this')]")
+        facebook_data['followers'] = (followers.find_element_by_xpath("./..").text)
+    except:
+        makeConnection(driver,link)
 
 def facebookCongress(link, local = False):
 
@@ -13,6 +21,15 @@ def facebookCongress(link, local = False):
     print(link)
     driver.get(f'https://facebook.com/{facebook_data["handle"]}')
     sleep(2)
+
+    try:
+        followers = driver.find_element_by_xpath("//*[contains(text(),'like this')]")
+        facebook_data['followers'] = (followers.find_element_by_xpath("./..").text.split(' '))
+    except:
+        makeConnection(driver,f'https://facebook.com/{facebook_data["handle"]}')
+
+
+    print(facebook_data['followers'])
 
     driver.close()
 
