b05e878611a2acb2a80c1a4dd04e5e62246b1d34
hamzsait <hamzasait@utexas.edu>
2021-06-06 18:07:03 -0500

    changed model of twitter to be an object with url, and handle

--- models/houseoftexas.js
+++ models/houseoftexas.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const texas_congress = new Schema({
   _id: { type: Number, required: true },
   name: { type: String, required: true },
-  twitter: { type: String, required: true },
+  twitter: {
+    url:{ type: String, required: true },
+    handle:{ type: String, required: true},
+  },
   facebook: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
