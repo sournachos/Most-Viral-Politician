1293b5be8d5b04acbee41a82a4a430ec2de74fdb
Matthew Parker <matt.parker1007@gmail.com>
2021-06-06 21:49:14 -0500

    made polprofile flexible

--- client/src/pages/PolProfile.js
+++ client/src/pages/PolProfile.js
@@ -30,32 +30,35 @@ export default function PolProfile() {
 
   return (
 
-    <>
+    <div className="container">
       <div className=" d-flex my-4 ">
-        <Card className=" p-2 w-25 me-4">
-          <Card.Body className="text-center">
-            <h2 className="text-center mb-4">{onepol.name}</h2>
-            <img style={{ width: "100%" }} src={onepol.image} />
-            {favoritePol()}
-          </Card.Body>
-        </Card>
+        <div className="row">
+          <Card className="col-md-3 col-sm-4 p-2">
+            <Card.Body className="text-center">
+              <h2 className="text-center mb-4">{onepol.name}</h2>
+              <img style={{ width: "100%" }} src={onepol.image} />
+              {favoritePol()}
+            </Card.Body>
+          </Card>
 
-        <Card className=" p-2 flex-grow-1">
-          <Card.Body>
-            <h2 className="text-center mb-4">Info</h2>
-            <ul>
-              <li><strong>Party:</strong> {onepol.party}</li>
-              <li><strong>District:</strong> {onepol._id}</li>
-              <li><strong>E-mail:</strong> <a href={"mailto:" + onepol.email}>{onepol.email}</a></li>
-              <li><strong>Phone:</strong> {onepol.phone}</li>
-              <li><strong>Twitter:</strong> <a href={onepol.twitter ? onepol.twitter.url: ''}>{onepol.twitter ? onepol.twitter.handle : ''}</a></li> 
-              <li><strong>Facebook:</strong> <a href={onepol.facebook}>{onepol.facebook}</a></li>
-            </ul>
-          </Card.Body>
-        </Card>
+          <Card className=" col-md-9 col-sm-8 p-2">
+            <Card.Body>
+              <h2 className="text-center mb-4">Info</h2>
+              <ul>
+                <li><strong>Party:</strong> {onepol.party}</li>
+                <li><strong>District:</strong> {onepol._id}</li>
+                <li><strong>E-mail:</strong> <a href={"mailto:" + onepol.email}>{onepol.email}</a></li>
+                <li><strong>Phone:</strong> {onepol.phone}</li>
+                <li><strong>Twitter:</strong> <a href={onepol.twitter ? onepol.twitter.url : ''}>{onepol.twitter ? onepol.twitter.handle : ''}</a></li>
+                <li><strong>Facebook:</strong> <a href={onepol.facebook}>{onepol.facebook}</a></li>
+              </ul>
+            </Card.Body>
+          </Card>
+        </div>
+
       </div>
 
-      <Card className="w-100  mb-2">
+      <Card className="row">
         <Card.Body>
           <h2 className="text-center mb-4">Stats</h2>
 
@@ -64,6 +67,6 @@ export default function PolProfile() {
         </Card.Body>
       </Card>
 
-    </>
+    </div>
   )
 }
